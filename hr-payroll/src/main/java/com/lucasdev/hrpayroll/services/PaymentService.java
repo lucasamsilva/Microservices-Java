53fd67239b55d5d0ba258bae1ec1c4f4d5af5526
lucasamsilva <lucasandreim280998@gmail.com>
2021-03-23 02:33:12 -0300

    Balanceamento de cargas com Ribbon

--- hr-payroll/src/main/java/com/lucasdev/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/lucasdev/hrpayroll/services/PaymentService.java
@@ -1,14 +1,22 @@
 package com.lucasdev.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasdev.hrpayroll.entities.Payment;
+import com.lucasdev.hrpayroll.entities.Worker;
+import com.lucasdev.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 	
 }
