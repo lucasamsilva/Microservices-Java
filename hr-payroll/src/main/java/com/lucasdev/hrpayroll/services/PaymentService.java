3e489793b538f863c34bb82055bb78f22481f4c9
lucasamsilva <lucasandreim280998@gmail.com>
2021-03-23 01:23:45 -0300

    hr-payroll implementado

---
+++ hr-payroll/src/main/java/com/lucasdev/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.lucasdev.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.lucasdev.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+	
+}
--- hr-worker/src/main/java/com/lucasdev/hrworker/repositories/WorkerRepository.java
+++ hr-worker/src/main/java/com/lucasdev/hrworker/repositories/WorkerRepository.java
@@ -1,9 +1,11 @@
 package com.lucasdev.hrworker.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lucasdev.hrworker.entities.Worker;
 
+@Repository
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
 
 }
